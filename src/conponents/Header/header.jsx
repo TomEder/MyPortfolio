7cdab06b4cee61fcd38a937d2362f381f53a548a
Tom Ederstål <tomderstal@gmail.com>
2022-11-17 22:14:31 +0100

    icons

--- src/conponents/Header/header.jsx
+++ src/conponents/Header/header.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import LI from "../../Images/linkedin.png";
+import GH from "../../Images/github.png";
+
+const LiImg = LI;
+const GHImg = GH;
 
 const Header = () => {
   return (
     <>
       <div className="mx-auto bg-tommy-background px-3 md:py-3">
         <div className="flex md:flex-row justify-between items-center">
-          <div className="space-x-8 md-block">
+          <div className="flex mt-5 space-x-8 md-block">
             <a
               rel="noreferrer"
               target="_blank"
               href="https://www.linkedin.com/in/tom-ederst%C3%A5l-5b3b99101/"
               className="text-base text-gray-500"
             >
-              LinkedIn
+              <img src={LiImg} alt="LinkedIn" />
             </a>
             <a
               rel="noreferrer"
@@ -20,7 +25,7 @@ const Header = () => {
               href="https://github.com/TomEder"
               className="text-base text-gray-500"
             >
-              GitHub
+              <img src={GHImg} alt="Github" />
             </a>
           </div>
         </div>
