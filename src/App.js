b7b2df34443e3df0a0647fefd836d1d2b5713ebb
TomEder <tomederstal@gmail.com>
2024-06-05 21:43:13 +0200

    removed unused vars

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import Header from "./conponents/Header/header";
 import Container from "./conponents/Container/container";
 import Projects from "./conponents/Projects/projects";
 import Footer from "./conponents/Footer/footer";
-import Skills from "./conponents/Skills/skills";
-import Divider  from "./conponents/Divider/Divider";
 import Services from "./conponents/Services/Services";
-
-import JsImg from "./Images/javascript-736400_640.png";
-import ReactImg from "./Images/ReactImg.png";
-import AngularImg from "./Images/AngularImg.png";
-import VueImg from "./Images/VueImg.png";
 import Landing from "./conponents/Landing/Landing";
 import Testimonials from "./conponents/Testimonials/Testimonials";
 import Contact from "./conponents/Contact/Contact";
--- src/conponents/Services/Services.jsx
+++ src/conponents/Services/Services.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHexagon } from '@fortawesome/free-solid-svg-icons';
 import "./Services.css";
 
 const Services = () => {
