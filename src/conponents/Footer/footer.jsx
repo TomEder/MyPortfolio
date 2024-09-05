1be8acd6f7261c1f5a857e6f0ccbb6d059ff2ca9
TomEder <tomederstal@gmail.com>
2024-09-05 10:27:45 +0200

    Update page title and footer text

--- src/conponents/Footer/footer.jsx
+++ src/conponents/Footer/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="text-center bg-tommy-background h-14">
-      <h2>© 2024 Ether Nordic. All rights reserved.</h2>
+      <h2>© 2024 EtherSteel Software. All rights reserved.</h2>
     </div>
   );
 };
