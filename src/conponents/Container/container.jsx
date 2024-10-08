73d2bdf5c57393e772e72e497ab183a5d25af1b9
TomEder <tomederstal@gmail.com>
2024-10-08 14:36:47 +0200

    Refactor font-family in App.css and container.jsx

--- src/conponents/Container/container.jsx
+++ src/conponents/Container/container.jsx
@@ -4,44 +4,49 @@ import { Link } from "react-scroll";
 
 const jagimg = Jag;
 
-const Container = () => {
-
+const About = () => {
   return (
-    <>
-      <div className="bg-gradient-to-t w-screen md:h-screen from-tommy-lower-bg to-tommy-background pt-10 flex flex-col md:flex-row items-center justify-between px-5 md:px-10 border-b-4">
-        <div className="text-center md:text-left md:w-1/2">
-          <h1 className="bg-transparent text-4xl md:text-6xl text-tommy-text overflow-hidden">
-            Tom Ederstål
-          </h1>
-          <p className="bg-transparent text-2xl md:text-4xl text-tommy-text mt-4 overflow-hidden">
-            Freelance web developer
-          </p>
-          <h2 className="text-base md:text-2xl my-3 md:my-5 text-tommy-text overflow-y-hidden">
-            I am a seasoned frontend developer with extensive experience in development and problem-solving. <br /> <br /> I possess an ambitious approach, complemented by a two-year specialized KYH diploma in frontend development and expertise in UX/UI design. My robust technical proficiency and interpersonal skills have made me well-liked by both colleagues and supervisors. As a team player, I also demonstrate the initiative and drive necessary for continuous personal and professional growth.
-          </h2>
-          <button className="mt-4 md:mt-5 bg-button-color hover:bg-button-hover text-white py-2 px-4 rounded shadow-sm" href>
+    <div className="w-screen h-screen bg-[#181818] flex flex-col md:flex-row items-center justify-between px-5 md:px-10 border-b-4">
+      <div className="text-center md:text-left md:w-1/2">
+        <h1 className="bg-transparent text-4xl md:text-6xl text-[#8A8A8A] overflow-hidden">
+          Tom Ederstål
+        </h1>
+        <p className="bg-transparent text-2xl md:text-4xl text-[#8A8A8A] mt-4 overflow-hidden">
+          Freelance web developer
+        </p>
+        <h2 className="text-base md:text-2xl my-3 md:my-5 text-[#8A8A8A] overflow-y-hidden">
+          With a two-year KYH diploma in frontend development and extensive
+          expertise in UX/UI design, I’ve built a solid technical foundation,
+          working at companies like Great-IT and Medexa Diagnostisk Service AB.
+          I’ve led the development of web applications using React, Angular,
+          TypeScript, and .NET. My technical skills, combined with strong
+          interpersonal abilities, have earned me respect from colleagues and
+          supervisors alike. I’m a team player with a proactive mindset,
+          constantly seeking opportunities for growth and staying on top of the
+          latest trends in web development.
+        </h2>
+        <button className="buttonStyle" href>
           <Link
-              to="contact"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              className="inline-block text-gray-400 no-underline hover:text-gray-200 hover:text-underline py-2 px-4 cursor-pointer"
-            >
-              Contact me!
-            </Link>
-          </button>
-        </div>
-        <div className="md:w-1/2 flex justify-center md:justify-end mt-8 md:mt-0">
-          <img
-            className="bg-transparent rounded-full w-40 h-40 md:w-60 md:h-60"
-            src={jagimg}
-            alt="Tom Ederstål"
-          />
-        </div>
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            className="inline-block text-2xl text-gray-400 no-underline hover:text-gray-200 hover:text-underline py-2 px-2 cursor-pointer"
+          >
+            Contact me!
+          </Link>
+        </button>
       </div>
-    </>
+      <div className="md:w-1/2 flex justify-center md:justify-end mt-8 md:mt-0">
+        <img
+          className="bg-transparent rounded-full w-40 h-40 md:w-80 md:h-80 border-2 mb-20 md:mb-0 md:mr-44 border-[#D35400]"
+          src={jagimg}
+          alt="Tom Ederstål"
+        />
+      </div>
+    </div>
   );
 };
 
-export default Container;
+export default About;
